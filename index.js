97ad66b8dacda8d1ffebf91793a0ae473b52296c
gjohnsx <93215236+gjohnsx@users.noreply.github.com>
2022-06-01 16:47:32 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let passwordInputs = document.querySelectorAll(".password");
+const possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_';
+
+let sliderValue = document.querySelector(".slider-value");
+let sliderValText = document.querySelector(".slider-text");
+
+function updateSlider(value) {
+    sliderValText.textContent = sliderValue.value;
+}
+
+function generatePasswordText() {
+    let passwordLength = sliderValue.value;
+    // let passwordLength = 16;
+    let password = '';
+    while (passwordLength > 0) {
+        password += possibleChars[Math.floor(Math.random() * possibleChars.length)];
+        passwordLength -= 1;
+    }
+    return password;
+}
+
+function generatePasswords() {
+    for (let i=0; i<passwordInputs.length; i++) {
+        passwordInputs[i].setAttribute("value", generatePasswordText());
+    }
+
+}
